b321e512e6529cb9bfd8d92088cda0c33342124f
puzznic <puzznic@inka.co.kr>
2025-07-24 14:47:41 +0900

    first commit

---
+++ Gitlab_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gitlab_Swift
+//
+//  Created by 박정근 on 7/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
